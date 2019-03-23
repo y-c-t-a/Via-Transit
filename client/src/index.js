a8cae9163e713876d5ff10de0b8a13535e0f0925
Jan-Strum <janusbased@gmail.com>
2019-03-23 14:19:00 -0500

    is works

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,17 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { ApolloProvider } from 'react-apollo'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-// import YelpCategorySearch from './components/YelpCategorySearch'
+import { persistCache } from 'apollo-cache-persist'
 import DirectionsMain from './components/DirectionsMain'
 import { resolvers } from './resolvers'
-// import UserSelectedBusinesses from './components/UserSelectedBusinesses'
 import YelpMain from './components/YelpMain'
 
 const cache = new InMemoryCache()
+
+persistCache({
+  cache,
+  storage: window.localStorage
+})
 
 cache.writeData({
   data: {
