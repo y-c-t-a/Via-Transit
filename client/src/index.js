922ff3bc10c4320c1c2f440381c9ab9dfbaece3a
Delilah Clement <delilahclement@gmail.com>
2019-03-25 08:19:55 -0500

    trying out persist cache

--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,17 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { ApolloProvider } from 'react-apollo'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-// import { persistCache } from 'apollo-cache-persist'
+import { persistCache } from 'apollo-cache-persist'
 import DirectionsMain from './components/DirectionsMain'
 import { resolvers } from './resolvers'
 import YelpMain from './components/YelpMain'
 
 const cache = new InMemoryCache()
 
-// persistCache({
-//   cache,
-//   storage: window.localStorage
-// })
+persistCache({
+  cache,
+  storage: window.localStorage
+})
 
 cache.writeData({
   data: {
