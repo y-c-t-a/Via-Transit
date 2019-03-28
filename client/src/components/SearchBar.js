5d5149ab9b33dff87904597d90b29551c1b47d62
Delilah Clement <delilahclement@gmail.com>
2019-03-28 11:59:38 -0500

    isolating the server

--- client/src/components/SearchBar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Input } from 'semantic-ui-react'
-export const AddressSearch = () => {
-  return <Input placeholder="Enter a start address..." />
-}
-
-export default AddressSearch
